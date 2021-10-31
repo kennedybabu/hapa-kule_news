cb3b55b0068545088e263d19ae09bd32b84d16f0
kennedybabu <kennedynjogu8@gmail.com>
2021-10-31 05:38:58 +0300

    add title to the routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from .config import DevConfig
 
 
 #Initialising application
 app = Flask(__name__)
+
+#setting up configuration
+app.config.from_object(DevConfig)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -6,13 +6,14 @@ def index():
     '''
     view root page function that returns the index page and its data
     '''
-
-    return render_template('index.html')
+    title = "Hapa Kule News - Home of your news needs"
+    return render_template('index.html', title = title)
 
 @app.route('/news/<int:news_id>')
 def news(news_id):
     '''
     View news page function that returns the  news details page and its data
     '''
-
-    return render_template('news.html', id = news_id)
+    
+    title = "Hapa Kule News - Home of your news needs"
+    return render_template('news.html', id = news_id, title = title)
