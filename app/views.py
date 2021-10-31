c8124fdf76eca2559c13d1a933381ad09cb29458
kennedybabu <kennedynjogu8@gmail.com>
2021-10-31 09:04:47 +0300

    display current time to the user

--- app/views.py
+++ app/views.py
@@ -1,16 +1,19 @@
 from flask import render_template
 from app import app
 from .request import get_news
+from datetime import date, datetime
 
 @app.route('/')
 def index():
     '''
     view root page function that returns the index page and its data
     '''
+    today = date.today()
+    current_date = today.strftime("%B %d, %Y")
     #getting general news
-    general_news = get_news('business')
+    general_news = get_news('sports')
     title = "Hapa Kule News - Home of your news needs"
-    return render_template('index.html', title = title, general = general_news)
+    return render_template('index.html', title = title, general = general_news, current_date = current_date)
 
 @app.route('/news/<int:news_id>')
 def news(news_id):
