2ad8829f9df93f18855d5ec6c4753dd0257766c2
kennedybabu <kennedynjogu8@gmail.com>
2021-10-31 05:29:02 +0300

    create the news route

--- app/views.py
+++ app/views.py
@@ -8,3 +8,11 @@ def index():
     '''
 
     return render_template('index.html')
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+    '''
+    View news page function that returns the  news details page and its data
+    '''
+
+    return render_template('news.html', id = news_id)
